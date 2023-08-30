cef5234dc1e3a6cb11f7e7c7e4a0c1d32976a70f
Shubham Singh <shubhammahar1306@gmail.com>
2023-08-30 12:12:32 +0530

    I added a io reader to read the data from body of the request

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
 		log.Println("I love GeeksforGeeks")
-})
+        
+		d, _ := ioutil.ReadAll(r.Body)
+        
+		log.Printf("Data : %s\n", d)
+
+		fmt.Fprintf(rw, "\nHello There %s", d)
+	})
 	http.ListenAndServe(":6000", nil)
 }
 
