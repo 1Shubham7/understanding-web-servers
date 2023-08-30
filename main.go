6a1b8fb578556743b70d866080b887ef127efe89
Shubham Singh <shubhammahar1306@gmail.com>
2023-08-30 12:11:21 +0530

    basic webser now I added a HTTP handler for path '/'

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("I love GeeksforGeeks")
+})
 	http.ListenAndServe(":6000", nil)
 }
+
