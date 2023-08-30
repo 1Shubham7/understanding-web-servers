c9d880fc663f3233db42f1d7fa7bee387269af85
Shubham Singh <shubhammahar1306@gmail.com>
2023-08-30 14:18:51 +0530

    added the error handling to the web server

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 )
 
 func main() {
+
+
 	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
 		log.Println("I love GeeksforGeeks")
-        
-		d, _ := ioutil.ReadAll(r.Body)
-        
-		log.Printf("Data : %s\n", d)
 
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err!= nil{
+			http.Error(rw, "Sorry, Try again later", http.StatusBadRequest)
+			return
+		}
+		log.Printf("Data : %s\n", d)
 		fmt.Fprintf(rw, "\nHello There %s", d)
 	})
 	http.ListenAndServe(":6000", nil)
