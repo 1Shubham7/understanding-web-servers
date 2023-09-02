494b6561d6a0359a69854ac6d95afe91f4fe23c5
Shubham Singh <shubhammahar1306@gmail.com>
2023-09-02 23:43:04 +0530

    complete: final version ready - added the graceful shutdown, timeouts with proper handler package

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"github.com/1shubham7/understanding-web-servers/handlers"
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"github.com/1shubham7/understanding-web-servers/handlers"
 	// "io"
 )
 
@@ -20,7 +24,31 @@ func main() {
 	servemux.Handle("/hello",hellohandler)
 	servemux.Handle("/bye", byehandler)
 
-	http.ListenAndServe(":6000", servemux)
+	server := &http.Server{
+		Addr: ":6000",
+		Handler: servemux,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 *time.Second,
+		WriteTimeout: 1 *time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	} ()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Performing Graceful shutdown", sig)
+
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(timeoutContext)
+	// http.ListenAndServe(":6000", servemux)
 	// second parameter is for http handler. if we say "nil" in second parameter, 
 	// the server will take the default http handler, here we specified the http handler
 
